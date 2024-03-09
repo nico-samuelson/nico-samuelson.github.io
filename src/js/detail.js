27614e275ec459a2073170faa2c86e98643ef315
nico-samuelson <nicost@Nicos-Laptop.local>
2024-03-09 23:50:14 +0700

    init commit

---
+++ src/js/detail.js
@@ -0,0 +1,64 @@
+const urlParams = new URLSearchParams(window.location.search);
+const workoutParam = urlParams.get("name");
+
+function updateUI(data) {
+  const workout = data.find((workout) => workout.slug == workoutParam);
+  console.log(workoutParam, workout);
+
+  const categories = workout.categories
+    .map(
+      (category) =>
+        ` <div class="bg-neutral text-primary text-sm md:text-lg py-2 px-5 mb-0 rounded-md">${
+          category[0].toUpperCase() + category.substring(1)
+        }</div>`
+    )
+    .join("");
+
+  $("#workout_image").attr("src", workout.image);
+  $("#workout_name").text(workout.name);
+  $("#categories").html(categories);
+  $("#description").html(workout.description);
+  $("#duration").text(workout.duration + " min");
+  $("#set").text(workout.set);
+
+  const equipment = workout.equipment
+    ? workout.equipment.map((equip) => `${equip}, `).join("")
+    : "No equipment needed";
+  $("#equipment").text(equipment);
+}
+
+var url =
+  "https://indexeddb-e5d4a-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json";
+var networkDataReceived = false;
+
+fetch(url)
+  .then(function (res) {
+    const contentType = res.headers.get("Content-Type");
+    if (contentType && contentType.includes("application/json")) {
+      return res.json();
+    } else {
+      throw new Error("Offline network error!");
+    }
+  })
+  .then(function (data) {
+    networkDataReceived = true;
+    console.log("From web", data);
+    updateUI(data);
+  })
+  .catch(function (err) {
+    console.log(err)
+    if (!caches.match(window.location.href)) {
+      window.location.href = "/offline.html";
+    }
+    else {
+      if ("indexedDB" in window) {
+        readAllData("workouts").then(function (data) {
+          if (!networkDataReceived) {
+            console.log("From cache", data);
+            updateUI(data);
+          }
+        });
+      }
+    }
+  });
+
---
+++ src/js/feed.js
@@ -0,0 +1,48 @@
+function clearCards() {
+  $('.workouts').empty()
+}
+
+function createCard(data) {
+  $('.workouts').append(`
+    <a href="/detail.html?name=${data.slug}">
+      <div class="flex flex-col w-full gap-5">
+        <figure><img class="rounded-box" src="${data.image}" alt="${data.name}" /></figure>
+        <p class="font-bold text-xl text-white">${data.name}</p>
+      </div>
+    </a>
+  `)
+}
+
+function updateUI(data) {
+  clearCards();
+  for (var i = 0; i < data.length; i++) {
+    createCard(data[i]);
+  }
+}
+
+var url = 'https://indexeddb-e5d4a-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json';
+var networkDataReceived = false;
+
+fetch(url)
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    networkDataReceived = true;
+    console.log('From web', data);
+    var dataArray = [];
+    for (var key in data) {
+      dataArray.push(data[key]);
+    }
+    updateUI(dataArray);
+  });
+
+if ('indexedDB' in window) {
+  readAllData('workouts')
+    .then(function(data) {
+      if (!networkDataReceived) {
+        console.log('From cache', data);
+        updateUI(data);
+      }
+    });
+}
