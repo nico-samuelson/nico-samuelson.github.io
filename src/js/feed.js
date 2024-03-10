a682bd074daf7ffdd6375fc5c084df25d6f19d42
Nico <nicos.t.65@gmail.com>
2024-03-10 18:19:46 +0700

    fix: idb not stored on first visit, prevent response from disk cache

--- src/js/feed.js
+++ src/js/feed.js
@@ -1,51 +1,111 @@
+var url =
+  "https://indexeddb-e5d4a-default-rtdb.asia-southeast1.firebasedatabase.app/workouts.json";
+var networkDataReceived = false;
+let workouts = [];
+
 function clearCards() {
-  $('.workouts').empty()
+  $(".workouts").empty();
 }
 
-function createCard(data) {
-  $('.workouts').append(`
-    <a href="/detail.html?name=${data.slug}">
-      <div class="flex flex-col w-full gap-5">
+function createCard(data, delay) {
+  $(".workouts").append(`
+    <a href="/detail.html?name=${data.slug}" data-aos="fade-right" data-aos-anchor-placement="center-bottom" data-aos-delay="${delay}">
+      <div class="flex flex-col w-full gap-5 workout">
         <figure><img class="rounded-box w-full h-[250px] object-cover" src="${data.image}" alt="${data.name}" /></figure>
         <p class="font-bold text-xl text-white">${data.name}</p>
       </div>
     </a>
-  `)
+  `);
 }
 
 function updateUI(data) {
   clearCards();
   for (var i = 0; i < data.length; i++) {
-    createCard(data[i]);
+    createCard(data[i], (i % 3) * 100);
   }
 }
 
-var url = 'https://indexeddb-e5d4a-default-rtdb.asia-southeast1.firebasedatabase.app/workouts.json';
-var networkDataReceived = false;
+function search(text) {
+  const filtered = workouts.filter((workout) => {
+    return workout.name.toLowerCase().includes(text);
+  });
+  updateUI(filtered);
+}
 
-fetch(url)
-  .then(function(res) {
-    return res.json();
-  })
-  .then(function(data) {
-    networkDataReceived = true;
-    console.log('From web', data);
-    var dataArray = [];
-    for (var key in data) {
-      dataArray.push(data[key]);
+function filterByCategories(category) {
+  if (category == "all") {
+    updateUI(workouts);
+    return;
+  }
+
+  const filtered = workouts.filter((workout) => {
+    return workout.categories.includes(category);
+  });
+  updateUI(filtered);
+}
+
+$(document).ready(function () {
+  AOS.init();
+
+  // filter by categories
+  $('input[name="categories"]').click(function () {
+    // remove was checked from other radios
+    $('input[name="categories"]').data("waschecked", false);
+    $('input[name="categories"]').next().removeClass("bg-primary text-white");
+    $('input[name="categories"]').next().addClass("bg-neutral text-primary");
+
+    var $radio = $(this);
+
+    // if this was previously checked
+    if ($radio.data("waschecked") == true) {
+      $radio.prop("checked", false);
+      $radio.data("waschecked", false);
+      $radio.next().removeClass("bg-primary text-white");
+      $radio.next().addClass("bg-neutral text-primary");
+    } else {
+      $radio.data("waschecked", true);
+      $radio.next().removeClass("bg-neutral text-primary");
+      $radio.next().addClass("bg-primary text-white");
+      filterByCategories($radio.val());
     }
-    dataArray.sort((a, b) => {
-      return a.name.localeCompare(b.name);
-    });
-    updateUI(dataArray);
   });
 
-if ('indexedDB' in window) {
-  readAllData('workouts')
-    .then(function(data) {
-      if (!networkDataReceived) {
-        console.log('From cache', data);
-        updateUI(data);
-      }
+  // search
+  $("#search_workout").on("change", function () {
+    var value = $(this).val().toLowerCase();
+    search(value);
+  });
+});
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    networkDataReceived = true;
+    console.log("From web", data);
+    if (workouts.length > 0) {
+      workouts = [];
+    }
+    for (var key in data) {
+      workouts.push(data[key]);
+      writeData('workouts', data[key])
+    }
+    workouts.sort((a, b) => {
+      return a.name.localeCompare(b.name);
     });
+    updateUI(workouts);
+  });
+
+if ("indexedDB" in window) {
+  readAllData("workouts").then(function (data) {
+    if (!networkDataReceived) {
+      console.log("From cache", data);
+      workouts = [];
+      workouts = data.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
+      updateUI(workouts);
+    }
+  });
 }
--- sw.js
+++ sw.js
@@ -1,23 +1,18 @@
-importScripts('/src/js/idb.js');
-importScripts('/src/js/utility.js');
+importScripts("/src/js/idb.js");
+importScripts("/src/js/utility.js");
 
-var CACHE_STATIC_NAME = 'static-v18';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var CACHE_STATIC_NAME = "static-v1";
+var CACHE_DYNAMIC_NAME = "dynamic-v2";
 var STATIC_FILES = [
-  '/',
-  '/index.html',
-  '/offline.html',
-  '/src/js/app.js',
-  '/src/js/feed.js',
-  '/src/js/idb.js',
-  '/src/js/promise.js',
-  '/src/js/fetch.js',
-  '/src/css/app.css',
-  '/src/css/feed.css',
-  'https://fonts.googleapis.com/css?family=Roboto:400,700',
-  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/src/js/app.js",
+  "/src/js/feed.js",
+  "/src/js/idb.js",
+  "/src/js/promise.js",
+  "/src/js/fetch.js",
   "https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js",
-  'https://cdn.jsdelivr.net/npm/daisyui@4.7.2/dist/full.min.css',
 ];
 
 // function trimCache(cacheName, maxItems) {
@@ -33,37 +28,38 @@ var STATIC_FILES = [
 //     })
 // }
 
-self.addEventListener('install', function (event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
   event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
-      .then(function (cache) {
-        console.log('[Service Worker] Precaching App Shell');
-        cache.addAll(STATIC_FILES);
-      })
-  )
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
+      console.log("[Service Worker] Precaching App Shell");
+      cache.addAll(STATIC_FILES);
+    })
+  );
 });
 
-self.addEventListener('activate', function (event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
+self.addEventListener("activate", function (event) {
+  console.log("[Service Worker] Activating Service Worker ....", event);
   event.waitUntil(
-    caches.keys()
-      .then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
           if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key);
+            console.log("[Service Worker] Removing old cache.", key);
             return caches.delete(key);
           }
-        }));
-      })
+        })
+      );
+    })
   );
   return self.clients.claim();
 });
 
 function isInArray(string, array) {
   var cachePath;
-  if (string.indexOf(self.origin) === 0) { // request targets domain where we serve the page from (i.e. NOT a CDN)
-    console.log('matched ', string);
+  if (string.indexOf(self.origin) === 0) {
+    // request targets domain where we serve the page from (i.e. NOT a CDN)
+    console.log("matched ", string);
     cachePath = string.substring(self.origin.length); // take the part of the URL AFTER the domain (e.g. after localhost:8080)
   } else {
     cachePath = string; // store the full request (for CDNs)
@@ -71,63 +67,55 @@ function isInArray(string, array) {
   return array.indexOf(cachePath) > -1;
 }
 
-self.addEventListener('fetch', function (event) {
-
-  var url = 'https://indexeddb-e5d4a-default-rtdb.asia-southeast1.firebasedatabase.app/workouts';
+self.addEventListener("fetch", function (event) {
+  var url =
+    "https://indexeddb-e5d4a-default-rtdb.asia-southeast1.firebasedatabase.app/workouts";
   if (event.request.url.indexOf(url) > -1) {
-    event.respondWith(fetch(event.request)
-      .then(function (res) {
-        // if (!res.ok) {
-        //   throw new Error('Network response was not ok.');
-        // }
-        var clonedRes = res.clone();  
-        // clearAllData('posts')
-        //   .then(function () {
-        //     return clonedRes.json();
-        //   })
-        clonedRes.json()
-          .then(function (data) {
+    event.respondWith(
+      fetch(event.request, { headers: { "Cache-Control": "no-cache" } })
+        .then(function (res) {
+          var clonedRes = res.clone();
+          // clearAllData('posts')
+          //   .then(function () {
+          //     return clonedRes.json();
+          //   })
+          clonedRes.json().then(function (data) {
+            console.log(data);
             for (var key in data) {
-              writeData('workouts', data[key])
+              writeData("workouts", data[key]);
             }
           });
-        return res;
-      })
-      .catch(function(err) {
-        console.log("fetch error: " + err)
-        return caches.match('/offline.html')
-      })
+          return res;
+        })
+        .catch(function (err) {
+          console.log("fetch error: " + err);
+          return caches.match("/offline.html");
+        })
     );
   } else if (isInArray(event.request.url, STATIC_FILES)) {
-    event.respondWith(
-      caches.match(event.request)
-    );
+    event.respondWith(caches.match(event.request));
   } else {
     event.respondWith(
-      caches.match(event.request)
-        .then(function (response) {
-          if (response) {
-            return response;
-          } else {
-            return fetch(event.request)
-              .then(function (res) {
-                return caches.open(CACHE_DYNAMIC_NAME)
-                  .then(function (cache) {
-                    // trimCache(CACHE_DYNAMIC_NAME, 3);
-                    cache.put(event.request.url, res.clone());
-                    return res;
-                  })
-              })
-              .catch(function (err) {
-                return caches.open(CACHE_STATIC_NAME)
-                  .then(function (cache) {
-                    if (event.request.headers.get('accept').includes('text/html')) {
-                      return cache.match('/offline.html');
-                    }
-                  });
+      caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request, {headers: { "Cache-Control": "no-cache" }})
+            .then(function (res) {
+              return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+                cache.put(event.request.url, res.clone());
+                return res;
               });
-          }
-        })
+            })
+            .catch(function (err) {
+              return caches.open(CACHE_STATIC_NAME).then(function (cache) {
+                if (event.request.headers.get("accept").includes("text/html")) {
+                  return cache.match("/offline.html");
+                }
+              });
+            });
+        }
+      })
     );
   }
 });
